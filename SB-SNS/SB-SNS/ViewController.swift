2fa304e9421d282cae731efdacc9964144bba8b6
Yoshiatsu-Irei <yoshiatsu.irei@fuller.co.jp>
2018-08-20 13:27:45 +0900

    SNSアプリ追加

---
+++ SB-SNS/SB-SNS/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SB-SNS
+//
+//  Created by 伊禮吉充 on 2018/08/20.
+//  Copyright © 2018年 伊禮吉充. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+}
+
