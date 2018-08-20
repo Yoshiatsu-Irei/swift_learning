75c0babe966d88cfd2008dbda71b10475444eb54
Yoshiatsu-Irei <yoshiatsu.irei@fuller.co.jp>
2018-08-20 17:56:49 +0900

    アプリ追加

---
+++ SB-NewsReader/SB-NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  SB-NewsReader
+//
+//  Created by 伊禮吉充 on 2018/08/20.
+//  Copyright © 2018年 伊禮吉充. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title = ""
+    var link = ""
+}
